544d56008b12544d3d82435e70c3d0b3a57b1e2a
Earlz <earlz@lastyearswishes.com>
2013-07-10 17:40:07 -0400

    I must be some kind of monster

--- TypeSafeHttp/IRequestBuilder.cs
+++ TypeSafeHttp/IRequestBuilder.cs
@@ -1,9 +1,33 @@
 using System;
 
-namespace Earlz.TypeSafeHttp
+namespace Earlz.TypeSafeHttp.IRequestBuilder
 {
-	public interface IRequestBuilder
+	public interface ICreateProtocol : ICreateMethod
 	{
+		ICreateMethod WithProtocol(string version);
 	}
+	public interface ICreateMethod
+	{
+		ICreateUrlPost Post();
+		ICreateUrlGet Get();
+	}
+	public interface ICreateUrlGet
+	{
+		INewGet Url(string url);
+	}
+	public interface ICreateUrlPost
+	{
+		INewPost Url(string url);
+	}
+
+	public interface INewGet
+	{
+
+	}
+	public interface INewPost
+	{
+
+	}
+
 }
 
--- TypeSafeHttp/RequestBuilder.cs
+++ TypeSafeHttp/RequestBuilder.cs
@@ -1,12 +1,51 @@
 using System;
+using Earlz.TypeSafeHttp.IRequestBuilder;
 
 namespace Earlz.TypeSafeHttp
 {
-	public class RequestBuilder : IRequestBuilder
+	/*
+	public class RequestBuilder : ICreateProtocol, ICreateUrl, INewState
 	{
-		public RequestBuilder()
+		HttpRequest Request=new HttpRequest();
+		#region ICreateUrl implementation
+
+		INewState ICreateUrl.At(string url)
 		{
+			Request.Url=url;
+			return this;
+		}
+
+		#endregion
+
+		#region ICreateProtocol implementation
+
+		ICreateMethod ICreateProtocol.WithProtocol(string version)
+		{
+			Request.ProtocolVersion="1.1";
+			return this;
+		}
+
+		#endregion
+
+		#region ICreateMethod implementation
+
+		ICreateUrl ICreateMethod.UsingMethod(string method)
+		{
+			Request.Method=method;
+			return this;
+		}
+
+		#endregion
+
+		public static ICreateProtocol Create()
+		{
+			return new RequestBuilder();
+		}
+		private RequestBuilder()
+		{
+			Request.ProtocolVersion="1.1";
 		}
 	}
+	*/
 }
 
